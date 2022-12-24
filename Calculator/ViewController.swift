b8ccd00b8df22d4d3c5989b65c518f3f80a4df57
mambetalieva <116174694+mambetalieva@users.noreply.github.com>
2022-12-24 16:32:55 +0600

    Update ViewController.swift

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -8,6 +8,62 @@
 import UIKit
 
 class ViewController: UIViewController {
+    //
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Каира on 24.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var firstNum: UITextField!
+    
+    @IBOutlet weak var secondNum: UITextField!
+    
+    
+    @IBOutlet weak var total: UILabel!
+    
+    
+    @IBAction func plus(_ sender: UIButton) {
+        let plus = Int(firstNum.text!)!
+        let plus1 = Int(secondNum.text!)!
+        total.text = String(plus + plus1)
+    }
+    
+    @IBAction func minus(_ sender: UIButton) {
+        let minus = Int(firstNum.text!)!
+        let minus1 = Int(secondNum.text!)!
+        total.text = String(minus - minus1)
+        
+    }
+    
+    @IBAction func divede(_ sender: UIButton) {
+        let divide = Int(firstNum.text!)!
+        let divide1 = Int(secondNum.text!)!
+        total.text = String(divide / divide1)
+    }
+    
+    
+    @IBAction func multiply(_ sender: UIButton) {
+        let multiply = Int(firstNum.text!)!
+        let multiply1 = Int(secondNum.text!)!
+        total.text = String(multiply * multiply1)
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,4 +72,7 @@ class ViewController: UIViewController {
 
 
 }
+
+
+
 
